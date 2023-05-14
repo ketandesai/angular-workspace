eceb7b96f6e9615ca75a21c2069a753b639fde83
Ketan Desai <desai.ketan@gmail.com>
2023-05-14 14:43:18 -0400

    add live reloading with libraries

---
+++ projects/component-lib/src/lib/index.ts
@@ -0,0 +1,4 @@
+
+export * from './component-lib.service';
+export * from './component-lib.component';
+export * from './component-lib.module';
--- projects/component-lib/src/public-api.ts
+++ projects/component-lib/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of component-lib
  */
 
-export * from './lib/component-lib.service';
-export * from './lib/component-lib.component';
-export * from './lib/component-lib.module';
+export * from './lib/index';
