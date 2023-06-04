7308a48dcdb64ff1f5aafec5b6f109f048ff848f
Ketan Desai <desai.ketan@gmail.com>
2023-06-04 14:47:45 -0400

    fix formControl reloading

--- projects/component-lib/src/stories/TextInput.stories.ts
+++ projects/component-lib/src/stories/TextInput.stories.ts
@@ -1,11 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { TextInputComponent } from '../lib/components/text-input/text-input.component';
-import { FormControl } from '@angular/forms';
+import {
+  FormControl,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
-import { moduleMetadata, applicationConfig} from '@storybook/angular';
-import {BrowserAnimationsModule} from  '@angular/platform-browser/animations';
+import { moduleMetadata, applicationConfig } from '@storybook/angular';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
 // More on how to set up stories at: https://storybook.js.org/docs/angular/writing-stories/introduction
+
+const fc = () =>
+  new FormControl('stuff');
+
 const meta: Meta<TextInputComponent> = {
   title: 'Example/TextInputComponent',
   component: TextInputComponent,
@@ -14,6 +21,7 @@ const meta: Meta<TextInputComponent> = {
     props: {
       backgroundColor: null,
       ...args,
+      control: fc?.(),
     },
   }),
   argTypes: {},
@@ -23,9 +31,9 @@ const meta: Meta<TextInputComponent> = {
     }),
     moduleMetadata({
       declarations: [],
-      imports: [],
+      imports: [MatInputModule, MatFormFieldModule, ReactiveFormsModule],
     }),
-  ]
+  ],
 };
 
 export default meta;
@@ -35,11 +43,6 @@ type Story = StoryObj<TextInputComponent>;
 export const Primary: Story = {
   args: {
     label: 'Some Test Label',
-    control: new FormControl('value'),
-    placeholder: "Placeholder Text",
-
+    placeholder: 'Placeholder Text',
   },
 };
-
-
-
