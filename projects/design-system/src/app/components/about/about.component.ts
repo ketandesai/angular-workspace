c2ff0e530bf908d927b259decac56c7552ee5320
Ketan Desai <desai.ketan@gmail.com>
2023-08-19 15:46:06 -0400

    update colors

--- projects/design-system/src/app/components/about/about.component.ts
+++ projects/design-system/src/app/components/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatSlideToggleModule],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
