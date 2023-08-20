9ca12084c42fc04077daa2d0d47cd03067180bcd
Ketan Desai <desai.ketan@gmail.com>
2023-08-19 20:01:35 -0400

    center classification banner

--- projects/design-system/src/app/app.component.ts
+++ projects/design-system/src/app/app.component.ts
@@ -4,11 +4,18 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { NavComponent } from './components/nav/nav.component';
+import { ClassificationComponent } from './components/classification/classification.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, LayoutComponent, NavComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    LayoutComponent,
+    NavComponent,
+    ClassificationComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
